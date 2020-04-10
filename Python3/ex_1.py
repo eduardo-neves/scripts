33249bf72d8ead412085e0b4cec7e575a57ef0d8
Eduardo <eduardocn1999@gmail.com>
2020-04-10 00:39:31 -0300

    Test commit

--- Python3/ex_1.py
+++ Python3/ex_1.py
@@ -5,14 +5,14 @@ class Pessoa:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    
+
     def maiorDeIdade(self, age):
         if self.age >= 18:
             return "maior de 18 anos"
         else:
             return "menor de 18 anos"
-            
-os.system('clear')
+
+os.system('cls')
 
 nome = input("Insira seu nome: ")
 idade = int(input("Insira a sua idade: "))
@@ -23,4 +23,3 @@ print("Seu nome é", p1.name)
 print("Sua idade é", p1.age, "anos")
 print("Você é", p1.maiorDeIdade(p1.age))
 print("")
-
